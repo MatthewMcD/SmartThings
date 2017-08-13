ee52d8ccf9c8666b91ce4bf51a04460e38fd45cd
Matthew McDermott <matthew@ableblue.com>
2017-08-13 11:26:15 -0500

    Updated to include an array of Switches and Lights

---
+++ smartapps/matthewmcd/motion-activated-dimmer.src/motion-activated-dimmer.groovy
@@ -0,0 +1,131 @@
+/**
+ *  Motion Activated Dimmer
+ *
+ *  Copyright 2017 Matthew McDermott
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Motion Activated Dimmer",
+    namespace: "MatthewMcD",
+    author: "Matthew McDermott",
+    description: "Improved motion activated light control for dimmers and switches.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("Turn on when motion detected:") {
+        input "themotion", "capability.motionSensor", required: true, title: "Where?"
+    }
+    section("Turn on these switches") {
+        input "switches", "capability.switch", required: false, multiple:true
+    }
+    section("Turn on these dimmers") {
+        input "dimmers", "capability.switchLevel", required: false, multiple:true
+        input "level", "number", required: true, title: "Dimming level?", range: 1..100, defaultValue: 10
+        input "rate", "number", required: true, title: "Dimming rate?", range: 1..10, defaultValue: 10
+    }
+    //section("Set the dimmer level") {
+    //Is there a better input?
+    //    input "level", "number", required: true, title: "Level?"
+    //}
+    //section("Dimming rate") {
+    //Does rate do anything?
+    //    input "rate", "number", required: true, title: "Rate?"
+    //}
+    //Add Timing - http://docs.smartthings.com/en/latest/smartapp-developers-guide/time-methods.html
+    section("Turn on between what times?") {
+        input "fromTime", "time", title: "From", required: false
+        input "toTime", "time", title: "To", required: false
+    }
+}
+
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+    subscribe(themotion, "motion.active", motionDetectedHandler)
+}
+
+// TODO: implement event handlers
+def motionDetectedHandler(evt) {
+    log.debug "motionDetectedHandler called: $evt"
+    if (switches){
+    	log.debug "will send the on() command to ${switches.size()} switches"
+    }
+    
+    if (dimmers){
+		log.debug "will send the level:$level and $rate command to ${dimmers.size()} dimmers"
+	}
+	//if the light is on, don't change it
+	//http://docs.smartthings.com/en/latest/ref-docs/device-ref.html#device-current-state
+    
+    def dimmerOn = isOn(dimmers)
+    
+    def switchOn = isOn(switches)
+    
+    
+    
+    boolean between = true;
+    log.debug "no current setting for time between: ${between.toString()}"
+    if ((fromTime) && (toTime)){
+    	between = timeOfDayIsBetween(fromTime, toTime, new Date(), location.timeZone)
+        log.debug "current setting for time $fromTime : $toTime and ${between.toString()}"
+    } else {
+    	log.debug "no current setting for time between: ${between.toString()}"
+    }
+    //Only change light if it is off and between the target times
+    log.debug "Current Status before changes : switchOn $switchOn and dimmerOn: $dimmerOn"
+    if ((between)&&(!switchOn)&&(!dimmerOn)){
+    	if (dimmers){
+        	log.debug "SetLevel(1) Before"
+        	dimmers.setLevel(1)
+	        log.debug "Send setLevel($level,$rate) command to ${dimmers.size()} dimmers"
+    	    dimmers.setLevel(level,rate)
+    	}
+        if (switches){
+    		log.debug "Send the on() command to ${switches.size()} switches"
+    		switches.on()
+        }
+    } else {
+       log.debug "Motion ignored because switches or dimmers are on."
+    }
+    
+}
+
+def isOn(arr){
+    // returns true if any devices in the map are on.
+    boolean retVal = false;
+    if (arr) {
+    	def currSwitches = arr.currentSwitch
+    	def onSwitches = currSwitches.findAll { switchVal ->
+        	switchVal == "on" ? true : false
+    	}
+
+    	log.debug "isOn function: ${onSwitches.size()} out of ${arr.size()} switches are on"
+    	retVal = onSwitches.size() > 0 //? true : false
+   }
+   return retVal
+}
